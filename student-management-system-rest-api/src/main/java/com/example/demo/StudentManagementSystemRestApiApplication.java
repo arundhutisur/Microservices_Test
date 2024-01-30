c526fdf90557055a2b8845f559d4aebecc62bba4
Arundhuti Sur <85032238+arundhutisur@users.noreply.github.com>
2024-01-30 16:10:43 +0530

    Add files via upload

---
+++ student-management-system-rest-api/src/main/java/com/example/demo/StudentManagementSystemRestApiApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.entity.*;
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.repo.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemRestApiApplication implements CommandLineRunner {
+	
+	private final StudentRepository studentRepository;
+	
+
+	public StudentManagementSystemRestApiApplication(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemRestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		studentRepository.save(new Student(UUID.randomUUID().toString(), "John", "CS"));
+		studentRepository.save(new Student(UUID.randomUUID().toString(), "Mary", "CS"));
+		studentRepository.save(new Student(UUID.randomUUID().toString(), "Susan", "IT"));
+	}
+
+}
